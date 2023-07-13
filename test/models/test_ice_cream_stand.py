c8d320a1c4e1b37effe1bc40487c7419ddfdfe70
Ana Carolina <acalmeidarm@gmail.com>
2023-07-12 21:51:07 -0300

    ajustes no codigo

--- test/models/test_ice_cream_stand.py
+++ test/models/test_ice_cream_stand.py
@@ -4,11 +4,11 @@ from unittest import TestCase
 class TestIceCreamStand(TestCase):
 
     def setUp(self):
-        self.flavors_list = ['Baunilha', 'Chocolate', 'Morango']
-        self.ice_cream_stand = IceCreamStand("Sorveteria Delícia", "Sorveteria", self.flavors_list)
+        self.flavors_list = ['Pistache', 'Amarula', 'Maracuja']
+        self.ice_cream_stand = IceCreamStand("Sorveteria ASC", "Sorveteria", self.flavors_list)
 
     def test_flavors_available_with_flavors(self):
-        expected_output = "\nNo momento temos os seguintes sabores de sorvete disponíveis:\n\t-Baunilha\n\t-Chocolate\n\t-Morango"
+        expected_output = "\nNo momento temos os seguintes sabores de sorvete disponíveis:\n\t-Pistache\n\t-Amarula\n\t-Maracuja"
         self.assertEqual(self.ice_cream_stand.flavors_available(), expected_output)
 
     def test_flavors_available_without_flavors(self):
@@ -17,17 +17,17 @@ class TestIceCreamStand(TestCase):
         self.assertEqual(self.ice_cream_stand.flavors_available(), expected_output)
 
     def test_find_flavor(self):
-        flavor = 'Chocolate'
+        flavor = 'Amarula'
         expected_output = f"Temos no momento {flavor}!"
         self.assertEqual(self.ice_cream_stand.find_flavor(flavor), expected_output)
 
     def test_find_flavor_available(self):
-        flavor = 'Baunilha'
+        flavor = 'Pistache'
         expected_output = f"Temos no momento {flavor}!"
         self.assertEqual(self.ice_cream_stand.find_flavor(flavor), expected_output)
 
     def test_find_flavor_unavailable(self):
-        flavor = 'Creme'
+        flavor = 'Chocolate'
         expected_output = f"Não temos no momento {flavor}!"
         self.assertEqual(self.ice_cream_stand.find_flavor(flavor), expected_output)
 
@@ -36,18 +36,18 @@ class TestIceCreamStand(TestCase):
         self.assertEqual(self.ice_cream_stand.find_flavor(None), expected_output)
 
     def test_add_flavor(self):
-        flavor = 'Strawberry'
+        flavor = 'Manga'
         expected_output = f"{flavor} adicionado ao estoque!"
         self.assertEqual(self.ice_cream_stand.add_flavor(flavor), expected_output)
 
     def test_add_flavor_existing(self):
-        flavor = 'Baunilha'
+        flavor = 'Amarula'
         expected_output = "Sabor já disponível!"
         self.assertEqual(self.ice_cream_stand.add_flavor(flavor), expected_output)
 
     def test_add_flavor_new(self):
-        flavor = 'Creme'
-        expected_output = "Creme adicionado ao estoque!"
+        flavor = 'Passas ao rum'
+        expected_output = "Passas ao rum adicionado ao estoque!"
         self.assertEqual(self.ice_cream_stand.add_flavor(flavor), expected_output)
         self.assertIn(flavor, self.ice_cream_stand.flavors)
 
